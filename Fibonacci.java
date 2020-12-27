ad8d9e49800d6e2240d17874724eb3b95b3e99b8
wingskim <67400214+wingskim@users.noreply.github.com>
2020-12-27 19:37:26 +0900

    Update Fibonacci.java

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,21 +3,12 @@ package my.test.study.kim_narae;
 import java.util.Scanner;
 
 public class Fibonacci{
-
-
     public static void main(String[] args) {
-
-
         int intput = 0;
-
         Scanner scanner = new Scanner(System.in);
         System.out.print("N번까지의 피보나치 수열을 계산합니다 :");
         String put = scanner.next();
-
-
-
         long[] Arr = new long[intput];
-
         try {
             intput = Integer.parseInt(put);
         }
@@ -44,7 +35,6 @@ public class Fibonacci{
             }
         } else if(intput<=-1){
             System.out.println("1보다 큰 수를 입력해 주세요.");
-
         }
             else {
             System.out.println("94번째 값부터는 숫자가 너무 커서 표현할 수 없습니다.");
@@ -52,12 +42,5 @@ public class Fibonacci{
             System.exit(0);
         }
         }
-
-
-
-
-
-
     }
-
 }
